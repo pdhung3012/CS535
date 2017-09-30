736e172ae06cd0c11a4e56fef0b4c6465b3d96c0
hungphd <hungphd@iastate.edu>
2017-09-30 16:50:37 -0500

    Add FNV Hash function

--- src/pa1/BloomFilterFNV.java
+++ src/pa1/BloomFilterFNV.java
@@ -1,17 +1,63 @@
 package pa1;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class BloomFilterFNV {
+
+	private static final long FNV1_64_INIT = 0xcbf29ce484222325L;
+	private static final long FNV1_PRIME_64 = 1099511628211L;
+
+	private HashSet<String> setSource;
+	private int bitsPerElement;
+	private HashMap<String,Integer> target;
+	
+
+	public BloomFilterFNV(int setSize, int bitsPerElement) {
+		setSource = new HashSet<String>(setSize);
+		this.bitsPerElement = bitsPerElement;
+		target=new HashMap<String, Integer>();
+	}
+
+	public static long hash64(byte[] data) {
+		return hash64(data, data.length);
+	}
+
+	public static long hash64(byte[] data, int length) {
+		long hash = FNV1_64_INIT;
+		for (int i = 0; i < length; i++) {
+			hash ^= (data[i] & 0xff);
+			hash *= FNV1_PRIME_64;
+		}
+
+		return hash;
+	}
+
+	public void add(String s) {
+		setSource.add(s);
+	}
+
+	public boolean appears(String s) {
+		return true;
+	}
+
+	public int filterSize() {
+		return 0;
+	}
+
+	public int numHashes() {
+		return 0;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//cod
-	}
-	
-	public BloomFilterFNV(int setSize, int bitsPerElement){
+//		String strInput = "hello world";
+//		long strHashValue = hash64(strInput.getBytes());
+//		System.out.println(strInput + "\t" + strHashValue);
+//		strHashValue = hash64(strInput.getBytes());
+//		System.out.println(strInput + "\t" + strHashValue);
 		
-	}
-	
-	public void add(String s){
+		BloomFilterFNV fnvFilter=new BloomFilterFNV(10, 8);
 		
 	}
 
