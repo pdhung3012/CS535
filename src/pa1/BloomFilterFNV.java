3016b927e67929f7374e77940e5da99621a61385
hungphd <hungphd@iastate.edu>
2017-10-01 02:38:13 -0500

    test BloomFilterData

--- src/pa1/BloomFilterFNV.java
+++ src/pa1/BloomFilterFNV.java
@@ -1,7 +1,15 @@
 package pa1;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.HashSet;
+
+import util.FileIO;
 
 public class BloomFilterFNV extends BloomFilter {
 
@@ -16,24 +24,25 @@ public class BloomFilterFNV extends BloomFilter {
 
 	public BloomFilterFNV(int setSize, int bitsPerElement) {
 		super(setSize, bitsPerElement);
-		setFilter=new HashSet<String>();
+		setFilter = new HashSet<String>();
+		setSource = new HashSet<String>();
 	}
 
 	public long hash64(byte[] data) {
 		return hash64(data, data.length);
 	}
-	
+
 	public long[] hashMultiple64(byte[] data) {
-		long originalResult= hash64(data, data.length);
-		long[] arrResult=new long[numberOfHashFunction];
-		for(int i=0;i<numberOfHashFunction;i++){
-			long newValue=originalResult+originalResult%i;
-			arrResult[i]=newValue;
+		long originalResult = hash64(data, data.length);
+		long[] arrResult = new long[numberOfHashFunction];
+		
+		for (int i = 1; i <= numberOfHashFunction; i++) {
+			long newValue = originalResult + originalResult % i;
+			arrResult[i-1] = newValue;
 		}
 		return arrResult;
-		
+
 	}
-	
 
 	public long hash64(byte[] data, int length) {
 		long hash = FNV1_64_INIT;
@@ -46,26 +55,27 @@ public class BloomFilterFNV extends BloomFilter {
 	}
 
 	public void add(String s) {
-		
+
 		setSource.add(s);
-		long[] arrResult=hashMultiple64(s.getBytes());
-		for(int i=0;i<arrResult.length;i++){
+		long[] arrResult = hashMultiple64(s.getBytes());
+		for (int i = 0; i < arrResult.length; i++) {
 			setFilter.add(String.valueOf(arrResult[i]));
 		}
-		
+
 	}
 
 	public boolean appears(String s) {
 
 		setSource.add(s);
-		long[] arrResult=hashMultiple64(s.getBytes());
-		boolean isAppeared=true;
-		for(int i=0;i<arrResult.length;i++){
-			if(!setFilter.contains(String.valueOf(arrResult[i]))){
-				isAppeared=false;
+		long[] arrResult = hashMultiple64(s.getBytes());
+		boolean isAppeared = true;
+		for (int i = 0; i < arrResult.length; i++) {
+			if (!setFilter.contains(String.valueOf(arrResult[i]))) {
+				isAppeared = false;
 				break;
-				
-			};
+
+			}
+			;
 		}
 		return isAppeared;
 	}
@@ -87,10 +97,62 @@ public class BloomFilterFNV extends BloomFilter {
 		// System.out.println(strInput + "\t" + strHashValue);
 
 		BloomFilterFNV fnvFilter = new BloomFilterFNV(10, 8);
+		String fpInput = "C:\\Users\\pdhung\\Documents\\GitHub\\CS535\\data\\pa1\\Relation2.txt";
+		String strInput = FileIO.readStringFromFile(fpInput);
+		String[] arrItem = strInput.split("\n");
+		boolean checkAppear = false;
+		String strConflict = "";
+		int indexConflict = -1;
+		int numberConflict=0;
+		for (int i = 0; i < arrItem.length; i++) {
+			String strContent = arrItem[i].split("\\s+")[0];
+			checkAppear = fnvFilter.appears(strContent);
+			if (checkAppear) {
+				strConflict = strContent;
+				indexConflict = i + 1;
+				numberConflict++;
+				System.out.println("Conflict position " + indexConflict + " "
+						+ strConflict);
+				//break;
+			}
+			fnvFilter.add(strContent);
+
+		}
+		if (numberConflict>0) {
+			System.out.println("Number conflict " + numberConflict);
+		} else {
+			System.out.println("No conflict");
+		}
 
 	}
 
-	
+	public static void writeObjectToFile(Object object, String objectFile,
+			boolean append) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(
+					new BufferedOutputStream(new FileOutputStream(objectFile,
+							append)));
+			out.writeObject(object);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public static Object readObjectFromFile(String objectFile) {
+		try {
+			ObjectInputStream in = new ObjectInputStream(
+					new BufferedInputStream(new FileInputStream(objectFile)));
+			Object object = in.readObject();
+			in.close();
+			return object;
+		} catch (Exception e) {
+			// e.printStackTrace();
+			return null;
+		}
+	}
 
 	@Override
 	public int[] hashFunction(String s) {
