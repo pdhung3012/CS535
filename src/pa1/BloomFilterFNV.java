6e8883040da14e7a5e5574aa7264364a2723a464
hungphd <hungphd@iastate.edu>
2017-10-01 15:03:10 -0500

    add FNV bloom

--- src/pa1/BloomFilterFNV.java
+++ src/pa1/BloomFilterFNV.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -15,6 +16,9 @@ public class BloomFilterFNV extends BloomFilter {
 
 	private static final long FNV1_64_INIT = 0xcbf29ce484222325L;
 	private static final long FNV1_PRIME_64 = 1099511628211L;
+
+	private static final long FNV_64_INIT = 0xcbf29ce484222325L;
+	private static final long FNV_64_PRIME = 0x100000001b3L;
 
 	private HashSet<String> setSource;
 	private int bitsPerElement;
@@ -26,9 +30,9 @@ public class BloomFilterFNV extends BloomFilter {
 		super(setSize, bitsPerElement);
 		setFilter = new HashSet<String>();
 		setSource = new HashSet<String>();
-		System.out.println("Filter size "+filterSize);
-		for(int i=0;i<filterSize;i++){
-			filter[i]=0;
+		System.out.println("Filter size " + filterSize);
+		for (int i = 0; i < filterSize; i++) {
+			filter[i] = 0;
 		}
 	}
 
@@ -36,42 +40,67 @@ public class BloomFilterFNV extends BloomFilter {
 		return hash64(data, data.length);
 	}
 
-	public long[] hashMultiple64(byte[] data) {
-		long originalResult = hash64(data, data.length);
-		long[] arrResult = new long[numberOfHashFunction];
-		
-		for (int i = 1; i <= numberOfHashFunction; i++) {
-			long newValue = originalResult + originalResult % i;
-			arrResult[i-1] = newValue;
-		}
-		return arrResult;
-
-	}
+//	public long[] hashMultiple64(byte[] data) {
+//		long originalResult = Math.abs(hash64(data, data.length));
+//		long[] arrResult = new long[numberOfHashFunction];
+//
+//		for (int i = 1; i <= numberOfHashFunction; i++) {
+//			long newValue = originalResult + originalResult % i;
+//			arrResult[i - 1] = newValue;
+//		}
+//		return arrResult;
+//
+//	}
 
 	public long hash64(byte[] data, int length) {
-		long hash = FNV1_64_INIT;
-		for (int i = 0; i < length; i++) {
-			hash ^= (data[i] & 0xff);
-			hash *= FNV1_PRIME_64;
-		}
+		// long hash = FNV1_64_INIT;
+		// for (int i = 0; i < length; i++) {
+		// hash ^= (data[i] & 0xff);
+		// hash *= FNV1_PRIME_64;
+		// }
+		//
+		// return hash;
 
-		return hash;
+		// System.out.println("aaaa");
+		long rv = FNV_64_INIT;
+		for (int i = 0; i < length; i++) {
+			rv ^= data[i];
+			rv *= FNV_64_PRIME;
+		}
+		return rv;
+	}
+
+	public static long hash64(final String k) {
+		long rv = FNV_64_INIT;
+		final int len = k.length();
+		for (int i = 0; i < len; i++) {
+			rv ^= k.charAt(i);
+			rv *= FNV_64_PRIME;
+		}
+		return rv;
 	}
 
 	public void add(String s) {
 
 		setSource.add(s);
-//		int[] arrResult = hashFunction(s);
-//		for (int i = 0; i < arrResult.length; i++) {
-//			
-//			filter[arrResult[i]]=1;
-//			//setFilter.add(String.valueOf(arrResult[i]));
-//		}
-		
-		long[] arrResult = hashMultiple64(s.getBytes());
+		int[] arrResult = hashFunction(s);
 		for (int i = 0; i < arrResult.length; i++) {
-			setFilter.add(String.valueOf(arrResult[i]));
+			try {
+				filter[arrResult[i]] = 1;
+			} catch (Exception ex) {
+				System.out.println(arrResult[i] + " err");
+				ex.printStackTrace();
+				throw new ArrayIndexOutOfBoundsException(ex.getMessage());
+
+			}
+
+			// setFilter.add(String.valueOf(arrResult[i]));
 		}
+
+		// long[] arrResult = hashMultiple64(s.getBytes());
+		// for (int i = 0; i < arrResult.length; i++) {
+		// setFilter.add(String.valueOf(arrResult[i]));
+		// }
 
 	}
 
@@ -81,21 +110,28 @@ public class BloomFilterFNV extends BloomFilter {
 		int[] arrResult = hashFunction(s);
 		boolean isAppeared = true;
 
-//		for (int i = 0; i < arrResult.length; i++) {
-//			if(filter[arrResult[i]]!=1){
-//				isAppeared = false;
-//				break;				
-//			}
-//		}
-		
 		for (int i = 0; i < arrResult.length; i++) {
-			if (!setFilter.contains(String.valueOf(arrResult[i]))) {
-				isAppeared = false;
-				break;
-
+			try {
+				if (filter[arrResult[i]] != 1) {
+					isAppeared = false;
+					break;
+				}
+			} catch (Exception ex) {
+				System.out.println(s + " " + i + " " + arrResult[i] + " error");
+				ex.printStackTrace();
+				throw new ArrayIndexOutOfBoundsException(ex.getMessage());
 			}
-			;
+
 		}
+
+		// for (int i = 0; i < arrResult.length; i++) {
+		// if (!setFilter.contains(String.valueOf(arrResult[i]))) {
+		// isAppeared = false;
+		// break;
+		//
+		// }
+		// ;
+		// }
 		return isAppeared;
 	}
 
@@ -115,51 +151,58 @@ public class BloomFilterFNV extends BloomFilter {
 		// strHashValue = hash64(strInput.getBytes());
 		// System.out.println(strInput + "\t" + strHashValue);
 
-		BloomFilterFNV fnvFilter = new BloomFilterFNV(10, 8);
-		String fpInput = "data\\pa1\\Relation2.txt";
-		String strInput = FileIO.readStringFromFile(fpInput);
-		String[] arrItem = strInput.split("\n");
-		boolean checkAppear = false;
-		String strConflict = "";
-		int indexConflict = -1;
-		int numberConflict=0;
-		for (int i = 0; i < arrItem.length; i++) {
-			String strContent = arrItem[i].split("\\s+")[0];
-			checkAppear = fnvFilter.appears(strContent);
-			if (checkAppear) {
-				strConflict = strContent;
-				indexConflict = i + 1;
-				numberConflict++;
-				System.out.println("Conflict position " + indexConflict + " "
-						+ strConflict);
-				//break;
-			}
-			fnvFilter.add(strContent);
+		BloomFilterFNV fnvFilter = new BloomFilterFNV(2000000, 8);
+		// String fpInput = "data\\pa1\\Relation2.txt";
+		// String strInput = FileIO.readStringFromFile(fpInput);
+		// String[] arrItem = strInput.split("\n");
+		// boolean checkAppear = false;
+		// String strConflict = "";
+		// int indexConflict = -1;
+		// int numberConflict=0;
+		// for (int i = 0; i < arrItem.length; i++) {
+		// String strContent = arrItem[i].split("\\s+")[0];
+		// checkAppear = fnvFilter.appears(strContent);
+		// if (checkAppear) {
+		// strConflict = strContent;
+		// indexConflict = i + 1;
+		// numberConflict++;
+		// System.out.println("Conflict position " + indexConflict + " "
+		// + strConflict);
+		// //break;
+		// }
+		// fnvFilter.add(strContent);
+		//
+		// }
+		// if (numberConflict>0) {
+		// System.out.println("Number conflict " + numberConflict);
+		// } else {
+		// System.out.println("No conflict");
+		// }
 
-		}
-		if (numberConflict>0) {
-			System.out.println("Number conflict " + numberConflict);
-		} else {
-			System.out.println("No conflict");
-		}
+		String strInput = "gcfdaacdbegbgaf";
+		String strHexa = "e5803b95054b6068";
+//		BigInteger bi = new BigInteger(strHexa, 16);
+//		System.out.println(bi);
+		System.out.println(fnvFilter.hash64(strInput));
+//		System.out.println(fnvFilter.hashFunction(strInput)[0]);
+		// System.out.println("Test long value of"+Long.parseLong(strHexa, 16));
 
 	}
 
-	
 	@Override
 	public int[] hashFunction(String s) {
 		// TODO Auto-generated method stub
-		byte[] data=s.getBytes();
-		long originalResult = hash64(data, data.length);
-		if(originalResult<0){
-			originalResult=originalResult*(-1);
-		}
-		//System.out.println(s+" original long "+originalResult);
+		byte[] data = s.getBytes();
+		long originalResult = Math.abs(hash64(data, data.length));
+		// if(originalResult<0){
+		// originalResult=originalResult*(-1);
+		// }
+	//	System.out.println(s + " original long " + originalResult);
 		int[] arrResult = new int[numHashes];
-		
+
 		for (int i = 1; i <= numHashes; i++) {
-			long newValue = originalResult + originalResult % i;
-			arrResult[i-1] =(int) newValue;
+			long newValue = originalResult % (filterSize - i);
+			arrResult[i - 1] = (int) newValue;
 		}
 		return arrResult;
 	}
