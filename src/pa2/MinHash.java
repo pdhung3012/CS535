59756de069d109d28bb4bfe42dfddb6fe6bbf560
hungphd <hungphd@iastate.edu>
2017-10-19 17:08:46 -0500

    Update MinHash.java

--- src/pa2/MinHash.java
+++ src/pa2/MinHash.java
@@ -2,6 +2,57 @@ package pa2;
 
 public class MinHash {
 
+	private String folder;
+	private int numPermutations;
+	
+	
+	
+	public MinHash(String folder,int numPermutations){
+		this.folder=folder;
+		this.numPermutations=numPermutations;
+	}
+	
+	
+	
+	public int getNumPermutations() {
+		return numPermutations;
+	}
+
+
+
+	public void setNumPermutations(int numPermutations) {
+		this.numPermutations = numPermutations;
+	}
+
+
+
+	public String[] allDocs(){
+		return null;
+	}
+	
+	public double extractJaccard(String file1,String file2){
+		double result=0;
+		return result;
+	}
+	
+	public int[] minHashSig(String fileName){
+		return null;
+	}
+	
+	public double approximateJaccard(String file1,String file2){
+		double result=0;
+		return result;
+	}
+	
+	public int[][] minHashMatrix(){
+		return null;
+	}
+	
+	public int numTerms(){
+		return 0;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
