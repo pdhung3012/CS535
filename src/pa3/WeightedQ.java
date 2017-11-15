5f2493f1e74779bd8646cded2873bc14e27c246f
Shruti <Shruti@shrutis-mbp-2.student.iastate.edu>
2017-11-14 20:53:40 -0600

    updated

--- src/pa3/WeightedQ.java
+++ src/pa3/WeightedQ.java
@@ -27,26 +27,12 @@ public class WeightedQ {
 				return (o2.getValue()).compareTo(o1.getValue());
 			}
 		});
-//		for (Map.Entry<String, Integer> entry : list) {
-//			System.out.println(entry.getKey() + " ==== " + entry.getValue());
-//		}
+
 		Entry tuple = list.get(0);
 		String key = (String) tuple.getKey();
 		this.queue.remove(key);
-//		for (Map.Entry<String, Integer> entry : this.queue.entrySet()) {
-//			System.out.println(entry.getKey() + " ==== " + entry.getValue());
-//		}
+
 		return tuple;
 	}
 
-//	public static void main(String[] args) {
-//		WeightedQ wq = new WeightedQ();
-//		wq.add("link1", 20);
-//		wq.add("link2", 30);
-//		wq.add("link3", 30);
-//		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(wq.queue.entrySet());
-//		
-//		
-//		System.out.println(wq.extract());
-//	}
 }
--- src/pa3/WikiCrawler.java
+++ src/pa3/WikiCrawler.java
@@ -9,17 +9,17 @@ import java.util.regex.Pattern;
 public class WikiCrawler {
 
 	// initialise the queue and visited list
-	Queue<GraphNode> queue = new LinkedList<GraphNode>();
-	Set<String> visited = new HashSet<String>();
-	Set<String> visitedIrrelevant = new HashSet<String>();
-	List<GraphNode> crawled = new LinkedList<GraphNode>();
-	HashMap<String, Integer> CrawlerForbiddenURL = new HashMap<String, Integer>();
+	private LinkedList<GraphNode> queue = new LinkedList<GraphNode>();
+	private Set<String> visited = new HashSet<String>();
+	private Set<String> visitedIrrelevant = new HashSet<String>();
+	private List<GraphNode> crawled = new LinkedList<GraphNode>();
+	private HashMap<String, Integer> forbiddenURLS = new HashMap<String, Integer>();
 
-	public String seedURL, fileName;
-	public String[] keyWords;
-	public static String BASE_URL = "https://en.wikipedia.org";
-	public Integer maxGraphNodes;
-	public static int countDownload = 0;
+	private String seedURL, fileName;
+	private String[] keyWords;
+	private static String BASE_URL = "https://en.wikipedia.org";
+	private Integer maxGraphNodes;
+	private static int counterDownload = 0;
 	private boolean isWeighted;
 
 	WikiCrawler(String seedURL, String[] keyWords, Integer maxGraphNodes, String fileName, boolean isWeighted) {
@@ -28,7 +28,7 @@ public class WikiCrawler {
 		for (int i = 0; i < keyWords.length; i++) {
 			this.keyWords[i] = this.keyWords[i].toLowerCase();
 		}
-		this.maxGraphNodes = maxGraphNodes - 1;
+		this.maxGraphNodes = maxGraphNodes;
 		this.fileName = fileName;
 		this.isWeighted = isWeighted;
 	}
@@ -36,28 +36,23 @@ public class WikiCrawler {
 	void crawl() {
 
 		try {
-
-			// initialse page to seedURL
-
 			excludeRobotTxt();
-			if (CrawlerForbiddenURL.containsKey("*")) {
-				System.out.println("The site forbids download of all pages through crawlers...hence exiting");
+			if (forbiddenURLS.containsKey("*")) {
+				System.out.println("Not allowed to download this page!");
 			}
-			URL url = new URL(BASE_URL + this.seedURL);
-			String parent = this.seedURL, graph = "";
-			int isRoot = 1;
+			String parent = this.seedURL;
 			visited.add(parent);
 
 			GraphNode.keyWords = this.keyWords.clone();
 			GraphNode seed = new GraphNode("", parent);
-			if (!CrawlerForbiddenURL.containsKey(parent)) {
+			if (!forbiddenURLS.containsKey(parent)) {
 				seed.downloadPagesAndLinks(BASE_URL);
 				queue.add(seed);
 			}
-			
+
 			while (!queue.isEmpty()) {
 				GraphNode node = queue.remove();
-				ProcessLinks(node);
+				processingLinks(node);
 
 				if (visited.size() > this.maxGraphNodes) {
 					break;
@@ -67,10 +62,10 @@ public class WikiCrawler {
 			while (!queue.isEmpty()) {
 				GraphNode node = queue.remove();
 
-				extractEdgesFromLinks(node);
+				extractEdgesFromatchs(node);
 			}
 
-			PrintWriter writer = new PrintWriter(this.fileName);
+			PrintWriter writer = new PrintWriter(new FileOutputStream(this.fileName), true);
 
 			writer.println((visited.size()));
 
@@ -81,8 +76,8 @@ public class WikiCrawler {
 
 		} catch (Exception e) {
 
-			// e.printStackTrace();
-			System.out.println(" Exception " + e.getLocalizedMessage());
+			e.printStackTrace();
+
 		}
 	}
 
@@ -90,17 +85,16 @@ public class WikiCrawler {
 		try {
 			GraphNode node = new GraphNode(BASE_URL, "/robots.txt");
 			node.downloadPage(BASE_URL);
-			int index = 0, i = 1;
+			int i = 1;
 			String[] lines = node.content.split("\n");
 			for (String line : lines) {
 				if (line.startsWith("disallow")) {
-
 					String forbiddenURL = "";
-					int start = line.indexOf(":") + 1;
-					int end = line.length();
-					forbiddenURL = line.substring(start, end).trim();
-					CrawlerForbiddenURL.put(forbiddenURL, i);
-					i++;
+					int startIndex = line.indexOf(":") + 1;
+					int endIndex = line.length();
+					forbiddenURL = line.substring(startIndex, endIndex).trim();
+					System.out.println("forbidden:"+forbiddenURL);
+					forbiddenURLS.put(forbiddenURL, ++i);
 				}
 			}
 		} catch (Exception e) {
@@ -109,12 +103,9 @@ public class WikiCrawler {
 		}
 	}
 
-	void ProcessLinks(GraphNode node1) {
+	void processingLinks(GraphNode node1) {
 		String parent = node1.child;
-		// final List<GraphNode> links = new LinkedList<GraphNode>();
-		// final List<GraphNode> edges = new LinkedList<GraphNode>();
 		final HashMap<String, GraphNode> uniqueEdges = new HashMap<String, GraphNode>();
-		int irrelevant = 0, relavent = 0;
 		for (String address : node1.links) {
 
 			if (visited.size() > this.maxGraphNodes) {
@@ -122,20 +113,17 @@ public class WikiCrawler {
 			}
 
 			address = address.replace("\"", "");
-			if (address != null && (!CrawlerForbiddenURL.containsKey(address))) {
-				// System.out.println(address); //revert
+			if (address != null && (!forbiddenURLS.containsKey(address))) {
 				if (!address.contains(":") && (!address.contains("#"))) {
-					// System.out.println(parent + " " + address);
 					GraphNode node = new GraphNode(parent, address);
 
 					if (!parent.equalsIgnoreCase(address)) {
 						if (!visited.contains(address) && !visitedIrrelevant.contains(address)) {
 
-							System.out.println("Downloading and processing " + countDownload++ + " " + address
+							System.out.println("Downloading and processing " + counterDownload++ + " " + address
 									+ "  No of Visited nodes -" + visited.size());
 
 							if (node.downloadPagesAndLinks(BASE_URL)) {
-								relavent++;
 								queue.add(node);
 								if (!uniqueEdges.containsKey(address)) {
 									uniqueEdges.put(address, node);
@@ -144,19 +132,16 @@ public class WikiCrawler {
 								visited.add(address);
 							} else {
 								visitedIrrelevant.add(address);
-								irrelevant++;
-
 							}
 						}
 					}
 				}
 			}
 		}
-		// queue.addAll(links);
-		// crawled.addAll(edges);
+
 	}
 
-	void extractEdgesFromLinks(GraphNode node) {
+	void extractEdgesFromatchs(GraphNode node) {
 
 		final HashMap<String, GraphNode> uniqueEdges = new HashMap<String, GraphNode>();
 		for (String address : node.links) {
@@ -177,17 +162,14 @@ public class WikiCrawler {
 
 	public static void main(String[] arg) {
 
-		String[] keywords = { "friends" };
+		String[] keywords = {"Tennis"};
 
-		WikiCrawler cr = new WikiCrawler("/wiki/Friends", keywords, 1000, "/friendswiki.txt", false);
+		WikiCrawler cr = new WikiCrawler("/wiki/Tennis", keywords, 100, "/data/pa3/tenniswiki.txt", false);
 
 		long startTime = System.currentTimeMillis();
 		cr.crawl();
 		long endTime = System.currentTimeMillis();
 		long timeTaken = endTime - startTime;
-		// System.out.println("time taken "+timeTaken/1000+" download
-		// time"+GraphNode.totalTime/1000);
-
 		System.out.println("Data Written to file");
 
 	}
@@ -196,83 +178,116 @@ public class WikiCrawler {
 class GraphNode {
 
 	private static final int BUFFER_SIZE = 1024;
+	String parent;
+	String child;
+	String content;
 	public static String[] keyWords;
-	static int count = 1;
-	String parent, child, content;
+	static int counter = 1;
 
 	List<String> links = new LinkedList<String>();
 	static double totalTime = 0;
 
-	GraphNode(String src, String link) {
-		parent = src;
-		child = link;
+	GraphNode(String parent, String child) {
+		this.parent = parent;
+		this.child = child;
 	}
 
-	boolean downloadPagesAndLinks(String baseURL) {
-
+	boolean downloadPage(String baseURL) {
+		URL urlstrm;
+		InputStream is = null;
 		try {
-			Matcher mLink;
-			Pattern pLink = null;
 
-			// String HTML_HREF_TAG_PATTERN =
-			// "\\s*(?i)href\\s*=\\s*(\"([^\"]*\")|'[^']*'|([^'\">\\s]+))";
+			if (counter % 200 == 0) {
+				Thread.sleep(10000);
+			}
+			counter++;
+
+			long startDownloadTime = System.currentTimeMillis();
+			urlstrm = new URL(baseURL + this.child);
+			is = urlstrm.openStream();
+			content = getString(is);
+			content = content.toLowerCase();
+			long endDownloadTime = System.currentTimeMillis();
+			long timeTaken = endDownloadTime - startDownloadTime;
+			totalTime += timeTaken;
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return true;
+
+	}
+
+	@SuppressWarnings("resource")
+	boolean downloadPagesAndLinks(String baseURL) {
+		InputStream isr = null;
+		BufferedReader br = null;
+		try {
+			Matcher match;
+			Pattern pattern = null;
+
 			String hrefPattern = "/wiki/(?:[A-Za-z0-9-._~!#$&'()*+,;=:@]|%[0-9a-fA-F]{2})*";
-			pLink = Pattern.compile(hrefPattern);
+			pattern = Pattern.compile(hrefPattern);
 
 			String subChild = child.substring(child.lastIndexOf("/") + 1);
 			URL urlraw = new URL(baseURL + "/w/index.php?title=" + subChild + "&action=raw");
-			InputStream isr = urlraw.openStream();
-			BufferedReader rbr = new BufferedReader(new InputStreamReader(isr));
+			isr = urlraw.openStream();
+			br = new BufferedReader(new InputStreamReader(isr));
 
 			String ln, raw = "";
-			while ((ln = rbr.readLine()) != null) {
+			while ((ln = br.readLine()) != null) {
 				raw += ln.toLowerCase();
 			}
-			isr.close();
-			rbr.close();
+
 			for (String str : keyWords) {
 				if (!raw.contains(str)) {
 					return false;
 				}
 			}
 
-			if (count % 100 == 0) {
-				Thread.sleep(5000);
+			if (counter % 100 == 0) {
+				Thread.sleep(10000);
 			}
-			count++;
+			counter++;
 
 			long startTime = System.currentTimeMillis();
 
 			URL urlStream = new URL(baseURL + this.child);
 
 			Set<String> keys = new HashSet<String>();
-			/*
-			 * for(String str: keyWords){ keys.add(str); }
-			 */
-			// System.out.println("Downloading "+count);
+
 			InputStream is = urlStream.openStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			br = new BufferedReader(new InputStreamReader(is));
 			String line = "";
 			content = "";
-			Boolean foundp = false;
+			Boolean isP = false;
 			while ((line = br.readLine()) != null) {
 
-				// line = line;
-
 				if (line.contains("<p>")) { // revert
-					foundp = true;
+					isP = true;
 				}
 
 				for (String str : keyWords) {
 					if (line.contains(str))
 						keys.remove(str);
 				}
-				if (foundp && line.contains("<a href=")) {
+				if (isP && line.contains("<a href=")) {
 
-					mLink = pLink.matcher(line);
+					match = pattern.matcher(line);
 
-					while (mLink.find()) {
-						String address = line.substring(mLink.start(0), mLink.end(0));// mLink.group(1);
+					while (match.find()) {
+						String address = line.substring(match.start(0), match.end(0));// match.group(1);
 						links.add(address);
 					}
 				}
@@ -287,59 +302,33 @@ class GraphNode {
 			 */
 		} catch (Exception e) {
 
-			// e.printStackTrace();
-			System.out.println("Exception during file download " + e.getLocalizedMessage());
+			e.printStackTrace();
+
 			return false;
-		}
-
-		return true;
-	}
-
-	Boolean downloadPage(String baseURL) {
-		try {
-
-			if (count % 100 == 0) {
-				Thread.sleep(5000);
+		} finally {
+			try {
+				isr.close();
+				br.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			count++;
 
-			long startTime = System.currentTimeMillis();
-
-			URL urlStream = new URL(baseURL + this.child);
-
-			// System.out.println("Downloading "+count);
-			InputStream is = urlStream.openStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
-			String line = "";
-			content = getString(is);
-			content = content.toLowerCase();
-			long endTime = System.currentTimeMillis();
-			long timeTaken = endTime - startTime;
-			totalTime += timeTaken;
-
-			is.close();
-
-		} catch (Exception e) {
-
-			System.out.println("Exception during file download " + e.getLocalizedMessage());
-			return false;
 		}
 
 		return true;
-
 	}
 
 	public static String getString(InputStream inputStream) throws IOException {
 		if (inputStream == null)
 			return null;
-		BufferedReader in = new BufferedReader(new InputStreamReader(inputStream), BUFFER_SIZE);
-		int charsRead;
+		BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+		int chars;
 		char[] copyBuffer = new char[BUFFER_SIZE];
 		StringBuffer sb = new StringBuffer();
-		while ((charsRead = in.read(copyBuffer, 0, BUFFER_SIZE)) != -1)
-			sb.append(copyBuffer, 0, charsRead);
+		while ((chars = in.read(copyBuffer, 0, BUFFER_SIZE)) != -1)
+			sb.append(copyBuffer, 0, chars);
 		in.close();
 		return sb.toString();
 	}
 }
-
