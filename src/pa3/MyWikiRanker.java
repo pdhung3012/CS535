dc1166782fbbc7fecc4bb51af9efa52f338db7d3
Shruti <Shruti@shrutis-mbp-2.student.iastate.edu>
2017-11-15 18:19:40 -0600

    add wiki ranker

--- src/pa3/MyWikiRanker.java
+++ src/pa3/MyWikiRanker.java
@@ -1,9 +1,25 @@
 package pa3;
 
+import java.io.File;
+
 public class MyWikiRanker {
 
-	public MyWikiRanker() {
-		
+	public static void main(String[] arg) {
+
+		String[] keywords = { "Computer", "Computer Science", "Algorithms", ""};
+		String seedUrl = "/wiki/Computer";
+
+		WikiCrawler cr = new WikiCrawler(seedUrl, keywords, 100,
+				"data" + File.separator + "pa3" + File.separator + "tenniswiki.txt", false);
+		long startTime = System.currentTimeMillis();
+		System.out.println("Crawling started at:" + startTime);
+		cr.crawl();
+		long endTime = System.currentTimeMillis();
+		System.out.println("Crawling ended at:" + endTime);
+		long timeTaken = endTime - startTime;
+		System.out.println("Total time taken to crawl :" + timeTaken);
+		System.out.println("Writing to file complete!");
+
 	}
 
 }
