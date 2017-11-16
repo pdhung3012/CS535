9142d6eca6eb50d3156b907b35e7762efb456c70
pdhung3012 <pdhung3012@gmail.com>
2017-11-16 16:36:16 -0600

    Complete WeightedQ

--- src/pa3/WeightedQ.java
+++ src/pa3/WeightedQ.java
@@ -15,12 +15,10 @@ import util.FileIO;
 
 public class WeightedQ {
 
-	public Map<String, Double> queue;
+	public LinkedHashMap<String, Double> queue;
 	private ArrayList<String> links;
 	private String strWebContent;
-	
-	
-	
+
 	public String getStrWebContent() {
 		return strWebContent;
 	}
@@ -29,11 +27,11 @@ public class WeightedQ {
 		this.strWebContent = strWebContent;
 	}
 
-	public Map<String, Double> getQueue() {
+	public LinkedHashMap<String, Double> getQueue() {
 		return queue;
 	}
 
-	public void setQueue(Map<String, Double> queue) {
+	public void setQueue(LinkedHashMap<String, Double> queue) {
 		this.queue = queue;
 	}
 
@@ -52,9 +50,9 @@ public class WeightedQ {
 
 	public void add(String key, Double value) {
 		this.queue.put(key, value);
-//		if (queue.size() > 1) {
-//
-//		}
+		// if (queue.size() > 1) {
+		//
+		// }
 
 	}
 
@@ -76,7 +74,7 @@ public class WeightedQ {
 	}
 
 	boolean downloadPagesAndLinks(String myURL) {
-		links=new ArrayList<String>();
+		links = new ArrayList<String>();
 		BufferedReader br = null;
 		try {
 			Matcher match;
@@ -100,11 +98,11 @@ public class WeightedQ {
 			String line = "";
 			// content = "";
 			Boolean isP = false;
-			StringBuilder sbContent=new StringBuilder();
-			HashSet<String> setLinks=new HashSet<String>();
+			StringBuilder sbContent = new StringBuilder();
+			HashSet<String> setLinks = new HashSet<String>();
 			while ((line = br.readLine()) != null) {
-
-				sbContent.append(line+"\n");
+			//	System.out.println(line);
+				sbContent.append(line + "\n");
 				if (line.contains("<p>")) {
 					isP = true;
 				}
@@ -116,21 +114,24 @@ public class WeightedQ {
 					while (match.find()) {
 						String address = line.substring(match.start(0),
 								match.end(0));
+					//	System.out.println(address);
 						setLinks.add(address);
 					}
 				}
 			}
-			
+
 			// logging end time of processing the page
-//			long endTime = System.currentTimeMillis();
-//			long timeTaken = endTime - startTime;
+			// long endTime = System.currentTimeMillis();
+			// long timeTaken = endTime - startTime;
 			// totalTime += timeTaken;
 			is.close();
-			strWebContent=sbContent.toString();
-			links=new ArrayList<String>();
-			for(String strItem:setLinks){
+			strWebContent = sbContent.toString();
+			// System.out.println("Link "+links.size());
+			links = new ArrayList<String>();
+			for (String strItem : setLinks) {
 				links.add(strItem);
 			}
+			//System.out.println("li: "+links.size());
 		} catch (Exception e) {
 
 			e.printStackTrace();
@@ -149,76 +150,138 @@ public class WeightedQ {
 		return true;
 	}
 
-	public void distWordAndWord(String word1, String word2) {
-
-	}
-
-	public void distWordAndSet(String word1, String word2) {
-
-	}
-	
-	public ArrayList<String> heuristicComputeWeightedQ(String urlInput,String fpInputTopics){
-		ArrayList<String> lstOutput=new ArrayList<String>();
-		ArrayList<String> lstWords=new ArrayList<String>();
-		lstWords=getLstWordsFromFile(fpInputTopics);
-		downloadPagesAndLinks(urlInput);
-		 
-		for(int i=0;i<links.size();i++){
-			if(isContainInURLLink(urlInput, lstWords)){
-				add(urlInput, 1.0);				
-			} else{
-				
-			};
-		}
+	public int distWordAndWord(String word1, String word2,String urlContent) {
+		int result=-1;
+		String contentProcess=urlContent;
+		ArrayList<Integer> lstIndexWord1=getAllIndexesOfWordInDocument(word1, contentProcess);
+		ArrayList<Integer> lstIndexWord2=getAllIndexesOfWordInDocument(word2, contentProcess);
+		int maxDistance=1000000;
+		int currentDistance=maxDistance;
 		
-		//get WeightedQ
-		lstOutput=new ArrayList<String>();
-		for(String str:queue.keySet()){
-			Double val=queue.get(str);
-			lstOutput.add(str+"\t"+val);
-		}
-		
-		
-		return lstOutput;
-	}
-	
-	public String getOneSpaceContent(String input){
-		String strResult="";
-		String[] arr=input.trim().split("\\s+");
-		for(int i=0;i<arr.length;i++){
-			if(!arr[i].isEmpty()){
-				strResult+=arr[i]+" ";
+		for(int l=0;l<lstIndexWord1.size();l++){
+			for(int k=0;k<lstIndexWord2.size();k++){
+				int candidate=Math.abs(k-l);
+				if(candidate<=currentDistance){
+					currentDistance=candidate;
+				}
 			}
 		}
-		strResult=strResult.trim();
+		result=currentDistance;
+		
+		return result;
+	}
+
+	public int distWordAndListOfWord(String word1, ArrayList<String> lstTopics,String urlContent) {
+		int maxDistance=1000000;
+		int currentDistance=maxDistance;
+		for(int i=0;i<lstTopics.size();i++){
+			String wordItem = getOneSpaceContent(lstTopics.get(i).trim()
+					.toLowerCase());
+			int dist=distWordAndWord(word1, wordItem, urlContent);
+			if(dist<=currentDistance){
+				currentDistance=dist;
+			}
+		}
+		return currentDistance;
+	}
+
+	public ArrayList<String> heuristicComputeWeightedQ(String urlInput,
+			String fpInputTopics) {
+		ArrayList<String> lstOutput = new ArrayList<String>();
+		ArrayList<String> lstWords = new ArrayList<String>();
+		lstWords = getLstWordsFromFile(fpInputTopics);
+		int indexTried = 0, maxTried = 100;
+		int minDistanceAccept=20;
+		boolean isDownloadSuccess = false;
+		while (!isDownloadSuccess && indexTried < maxTried) {
+			isDownloadSuccess = downloadPagesAndLinks(urlInput);
+			indexTried++;
+		//	System.out.println("try "+indexTried);
+		}
+		
+		String contentProcess=strWebContent.trim().toLowerCase().replaceAll(
+				":", " ").replaceAll("_", " ").replaceAll("\\(", " ").replaceAll("\\)", " ").trim();
+		contentProcess=getOneSpaceContent(contentProcess);
+
+		for (int i = 0; i < links.size(); i++) {
+			//System.out.println(links.get(i));
+			//System.out.println(lstWords.toString());
+			if (isContainInURLLink(links.get(i), lstWords)) {				
+				add(links.get(i), 1.0);
+			} else {
+				int minDist=distWordAndListOfWord(links.get(i), lstWords, contentProcess);
+				double weighti=0;
+				if(minDist<=minDistanceAccept){
+					weighti=1.0/(minDist+2);
+					add(links.get(i), weighti);
+				} else{
+					add(links.get(i), 0.0);
+				}
+			}
+			;
+		}
+
+		// get WeightedQ
+		lstOutput = new ArrayList<String>();
+		for (String str : queue.keySet()) {
+			Double val = queue.get(str);
+			lstOutput.add(str + "\t" + val);
+		}
+
+		return lstOutput;
+	}
+
+	public String getOneSpaceContent(String input) {
+		String strResult = "";
+		String[] arr = input.trim().split("\\s+");
+		for (int i = 0; i < arr.length; i++) {
+			if (!arr[i].isEmpty()) {
+				strResult += arr[i] + " ";
+			}
+		}
+		strResult = strResult.trim();
 		return strResult;
 	}
 	
-	public boolean isContainInURLLink(String strInput,ArrayList<String> lstWords){
-		boolean isContain=false;
-		String[] arrInput=strInput.trim().split("/");
-		String strLinkContent=arrInput[arrInput.length-1];
-		String strContentSpace=getOneSpaceContent(strLinkContent.replaceAll(":"," ").replaceAll("_"," "));
-				
-		for(int i=0;i<lstWords.size();i++){
-			String word=getOneSpaceContent(lstWords.get(i).trim().toLowerCase());
-			if(strContentSpace.contains(word)){
-				isContain=true;
+	public ArrayList<Integer> getAllIndexesOfWordInDocument(String word,String urlContent){
+		ArrayList<Integer> lstResult=new ArrayList<Integer>();
+		int index = urlContent.indexOf(word);
+		while(index >= 0) {
+		    lstResult.add(index);
+			index = urlContent.indexOf(word, index+1);
+		    
+		}
+		return lstResult;
+	}
+
+	public boolean isContainInURLLink(String strInput,
+			ArrayList<String> lstWords) {
+		boolean isContain = false;
+		String[] arrInput = strInput.trim().split("/");
+		String strLinkContent = arrInput[arrInput.length - 1];
+		String strContentSpace = getOneSpaceContent(strLinkContent.replaceAll(
+				":", " ").replaceAll("_", " ").replaceAll("\\(", " ").replaceAll("\\)", " ").toLowerCase());
+		//System.out.println("content "+strContentSpace);
+		for (int i = 0; i < lstWords.size(); i++) {
+			String word = getOneSpaceContent(lstWords.get(i).trim()
+					.toLowerCase());
+			if (strContentSpace.contains(word)) {
+				isContain = true;
 				break;
 			}
 		}
-		
-		
+
 		return isContain;
 	}
-	
-	private ArrayList<String> getLstWordsFromFile(String fpInput){
-		String[] arrWords=FileIO.readStringFromFile(fpInput).trim().split("\n");
-		ArrayList<String> lstWords=new ArrayList<String>();
-		for(int i=0;i<arrWords.length;i++){
-			String item=arrWords[i].trim().toLowerCase();
+
+	private ArrayList<String> getLstWordsFromFile(String fpInput) {
+		String[] arrWords = FileIO.readStringFromFile(fpInput).trim()
+				.split("\n");
+		ArrayList<String> lstWords = new ArrayList<String>();
+		for (int i = 0; i < arrWords.length; i++) {
+			String item = arrWords[i].trim().toLowerCase();
 			lstWords.add(item);
-			//if()
+			// if()
 		}
 		return lstWords;
 	}
@@ -228,18 +291,22 @@ public class WeightedQ {
 		String urlStartWikiPage = urlPrefix + "wiki/Tennis";
 		String fpSetOfTopic = "data" + File.separator + "pa3" + File.separator
 				+ "TopicForWeightedQ.txt";
-		String fpOutputText="data" + File.separator + "pa3" + File.separator
+		String fpOutputText = "data" + File.separator + "pa3" + File.separator
 				+ "ResultWeightQ.txt";
-		WeightedQ wq=new WeightedQ();
-		ArrayList<String> lstOutput=wq.heuristicComputeWeightedQ(urlStartWikiPage, fpSetOfTopic);
-		StringBuilder sb=new StringBuilder();
-		sb.append("Craw from :"+urlStartWikiPage+"\n");
-		sb.append("Set of topics :"+wq.getLstWordsFromFile(fpSetOfTopic)+"\n");
-		sb.append("Num of results :"+lstOutput.size()+"\n");
-		for(int i=0;i<lstOutput.size();i++){
-			//System.out.println(i+"\t"+lstLinks.get(i));
+		WeightedQ wq = new WeightedQ();
+		ArrayList<String> lstOutput = wq.heuristicComputeWeightedQ(
+				urlStartWikiPage, fpSetOfTopic);
+		StringBuilder sb = new StringBuilder();
+		sb.append("Craw from :" + urlStartWikiPage + "\n");
+		sb.append("Set of topics :" + wq.getLstWordsFromFile(fpSetOfTopic)
+				+ "\n");
+		sb.append("Num of results :" + lstOutput.size()+"/"+wq.getLinks().size() + "\n");
+		for (int i = 0; i < lstOutput.size(); i++) {
+			// System.out.println(i+"\t"+lstLinks.get(i));
+			sb.append(lstOutput.get(i) + "\n");
 		}
-		
+		FileIO.writeStringToFile(sb.toString(), fpOutputText);
+		System.out.println("Check the result in " + fpOutputText);
 
 	}
 
